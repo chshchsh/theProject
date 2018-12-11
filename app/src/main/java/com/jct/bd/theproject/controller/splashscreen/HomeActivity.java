bd58448e67c814b2dd053be1766b9efcffae0256
bdelkayam <bdelkayam@gmail.com>
2018-12-12 01:39:17 +0200

    c

--- app/src/main/java/com/jct/bd/theproject/controller/splashscreen/HomeActivity.java
+++ app/src/main/java/com/jct/bd/theproject/controller/splashscreen/HomeActivity.java
@@ -2,7 +2,6 @@ package com.jct.bd.theproject.controller.splashscreen;
 
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
